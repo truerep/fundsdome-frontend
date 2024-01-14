61e1beb816e30afe0ea424baa7b83db0d71e6c8a
tradestealer999 <business.truerep@gmail.com>
2024-01-14 16:25:12 +0530

    setting cookie

--- api/authentication/loginUser.js
+++ api/authentication/loginUser.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import Cookies from 'universal-cookie';
 
 import { appEnv } from '@/helpers';
 
 const loginUser = async (username, password) => {
+  const cookies = new Cookies();
   try {
     const response = await axios.post(`${appEnv.apiUrl}/users/login`, {
       username,
@@ -11,6 +13,13 @@ const loginUser = async (username, password) => {
 
     const { token } = response.data;
     localStorage.setItem('token', token);
+    const setAuthTokenCookie = (token) => {
+      cookies.set('auth_token', {token}, { path: '/', domain: '.fundsdome.*' });
+    };
+  
+    setAuthTokenCookie(token);
+    const authToken = cookies.get('auth_token', { domain: '.fundsdome.com' });
+    console.log(authToken, "<--authToken from cookies")
     return token;
   } catch (error) {
     console.error('Authentication failed', error);
