6a5826f747f4fec8c3214dfd00e643ed43b6f461
tradestealer999 <business.truerep@gmail.com>
2024-01-14 17:38:34 +0530

    build 2.1

--- api/authentication/loginUser.js
+++ api/authentication/loginUser.js
@@ -14,11 +14,11 @@ const loginUser = async (username, password) => {
     const { token } = response.data;
     localStorage.setItem('token', token);
     const setAuthTokenCookie = (token) => {
-      cookies.set('auth_token', {token}, { path: '/', domain: '.fundsdome.*' });
+      cookies.set('auth_token', {token}, { path: '/', domain: 'fundsdome.com' });
     };
   
     setAuthTokenCookie(token);
-    const authToken = cookies.get('auth_token', { domain: '.fundsdome.com' });
+    const authToken = cookies.get('auth_token', { domain: 'fundsdome.com' });
     console.log(authToken, "<--authToken from cookies")
     return token;
   } catch (error) {
